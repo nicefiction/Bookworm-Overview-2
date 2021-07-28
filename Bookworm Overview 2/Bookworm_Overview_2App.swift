d33ab344135d80c2b5562c56c74c79d2a6ab3e0b
Olivier Van hamme <nicefiction@gmail.com>
2021-07-28 09:22:02 +0200

    Initial Commit

---
+++ Bookworm Overview 2/Bookworm_Overview_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Bookworm_Overview_2App.swift
+//  Bookworm Overview 2
+//
+//  Created by Olivier Van hamme on 28/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm_Overview_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
